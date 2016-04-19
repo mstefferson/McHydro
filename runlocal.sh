2b3a2f8baa326e7d4ccb649b7d6ca13f9bc10a00
Michael Stefferson <mstefferson@gmail.com>
2016-04-19 10:21:24 -0600

    Edited runlocal to exit matlab if there is an error and print the exit message. Runs now come with an output.log

--- runlocal.sh
+++ runlocal.sh
@@ -1,7 +1,11 @@
+# Bash executable
 #!/bin/bash
+
+# On mac, give path to matlab
 MATLAB=/Applications/MATLAB_R2015b.app/bin/matlab
 echo "Starting run"
 echo "In dir `pwd` "
-$MATLAB -nodesktop -r "run_bindobs; exit;"
-echo "Finished"
+$MATLAB -nodesktop -nosplash -logfile output.log \
+  -r  "try, run_bindobs, catch, exit(1), end, exit(0);" 
+echo "Finished. Matlab exit code: $?" 
 exit
