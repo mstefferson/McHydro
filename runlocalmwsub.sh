79a766fe6a4b3b2dcd9ce091a7ed6b870c5ba254
Michael Stefferson <mstefferson@gmail.com>
2016-04-25 10:36:57 -0600

    Added a runlocal bash runlocalmwsub.sh for my ubuntu. It works

---
+++ runlocalmwsub.sh
@@ -0,0 +1,11 @@
+# Bash executable
+#!/bin/bash
+
+# On mac, give path to matlab
+
+echo "Starting run"
+echo "In dir `pwd` "
+matlab -nodesktop -nosplash -logfile output.log \
+  -r  "try, run_bindobs, catch, exit(1), end, exit(0);" 
+echo "Finished. Matlab exit code: $?" 
+exit
