83db1aa7f142485d0f96f842b5af58cbaba47637
Michael Stefferson <mstefferson@gmail.com>
2016-05-02 09:57:30 -0600

    Correct for a user input error on the record times. If the recording chunk is larger than the total time, it changes recording chunk to the run time. This check is also done with rec_interval and rec_chunk

--- masterparams_bindobs.m
+++ masterparams_bindobs.m
@@ -6,7 +6,7 @@
 
 %key parameters and constants
 slide_barr_height=0;    %barrier height to sliding, in kT
-bind_energy_vec = [1];
+bind_energy_vec = [0];
 ffrac_obst_vec= [ 0.1 ];         %filling fraction of obstacles
 ffrac_tracer=0.1;       %filling fraction of tracers
 
@@ -17,38 +17,36 @@ params.ffrac_obst_vec = ffrac_obst_vec;
 params.ffrac_tracer = ffrac_tracer;
 
 %grid stuff
-const.n_trials       = 1;
-const.n_gridpoints   = 100;    % number of grid points, same in x and y
-const.ntimesteps     = 1e2;   % number of timesteps Note 1e5 gives errors on my laptop.
-const.rec_interval   = 6;    % time elapsed before recording
-const.rec_chunk      = 25;   % time elapsed before writing to file
-const.twait          = 1;    % time waited before recording
+const.n_trials      = 4;
+const.n_gridpoints  = 100;    % number of grid points, same in x and y
+const.ntimesteps    = 1e5;   % number of timesteps Note 1e5 gives errors on my laptop.
+const.rec_interval  = 1e2;    % time elapsed before recording
+const.rec_chunk     = 1e6;   % time elapsed before writing to file
+const.twait         = 1;    % time waited before recording
 const.TrRecFlag     = 1;     % Flag to record tracers or not
 const.ObsRecFlag    = 0;     % Flag to record obstacles or not
+const.size_obst     = 1;      %obstacle linear dimension, MUST BE odd integer
+const.size_tracer   = 1;     %tracer linear dimension, MUST BE odd integer
 
 %trial master
 trialmaster.tind       = 1;
 trialmaster.runstrtind = 1;
 trialmaster.nt         = const.n_trials;
 
-%other constants and model options
-const.size_obst=1;      %obstacle linear dimension, MUST BE odd integer
-const.size_tracer=1;     %tracer linear dimension, MUST BE odd integer
-modelopt.obst_excl=1;       %1 if obstacles sterically exclude each other, 0 if not
-modelopt.tracer_excl=0;     %MUST BE 0 so tracers don't interact (ghosts)
-modelopt.obst_trace_excl=0;  %1 if obstacles and tracers mutually exclude
-modelopt.dimension=2; %system dimension, currently must be 2
-
-const.nequil=0;           %number of timesteps for initial equilibration
-
 %model stuff
 modelopt.animate=0;          %1 to show animation, 0 for no animation
 modelopt.tpause=0.0;         %pause time in animation, 0.1 s is fast, 1 s is slow
 modelopt.movie=0;           %1 to record movie
-
+modelopt.obst_excl=1;       %1 if obstacles sterically exclude each other, 0 if not
+modelopt.tracer_excl=0;     %MUST BE 0 so tracers don't interact (ghosts)
+modelopt.obst_trace_excl=0;  %1 if obstacles and tracers mutually exclude
+modelopt.dimension=2;    %system dimension, currently must be 2
 
 % Fix time stuff and add some calculated things
 if const.twait < 1; const.twait = 1; end;
+if const.rec_chunk > const.ntimesteps; const.rec_chunk = const.ntimesteps; end;
+if const.rec_interval > const.rec_chunk; const.rec_interval = const.rec_chunk; end;
+
 const.rec_chunk  = round( const.rec_chunk / const.rec_interval ) * const.rec_interval; % fix rec_chunk
 const.NrecChunk  = const.rec_chunk / const.rec_interval; % Number of recorded points / chunk
 const.ntimesteps = floor( const.ntimesteps / const.rec_chunk ) * const.rec_chunk; % fix ntimesteps
@@ -57,5 +55,9 @@ const.Nchunklost = ceil( const.twait / const.rec_chunk) - 1; % Number of chunks 
 const.twait      = (const.Nreclost + 1) * const.rec_interval; % fix wait time. Add 1 to start recording at twait
 const.NrecTot    = const.ntimesteps / const.rec_interval - const.Nreclost; % Number of recorded points
 const.NchunkTot  = const.ntimesteps / const.rec_chunk - const.Nchunklost; % Number of recorded points
+
+% Number of particles on the grid
+const.num_tracer    = ffrac_tracer .* const.n_gridpoints ^ 2;
+
 
 save('Params')% Fix time stuff and add some calculated things
