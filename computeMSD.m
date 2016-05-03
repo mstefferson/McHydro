cd2312f973e5c174cd752a41b927dbdd979ba0ea
Michael Stefferson <mstefferson@gmail.com>
2016-05-03 16:49:25 -0600

    Fixed time indexing error in compute_msd

--- computeMSD.m
+++ computeMSD.m
@@ -11,8 +11,10 @@ function [msd,dtime]=computeMSD(x, maxpts_msd, quadFlag)
 %  msd(:,4)=mean quartic displacement
 %  msd(:,5)=std(quartic displacement)
 
-number_delta_t=size(x,3);
-dtime=[0:number_delta_t-1]';
+number_timepnts = size(x,3);
+number_delta_t  = number_timepnts - 1;
+dtime= ( 1 : number_delta_t)' ;
+
 if quadFlag
    msd_local=zeros(number_delta_t,5); %Store [mean, std, n]
 else
@@ -23,12 +25,12 @@ end
 msd_distrib=msd_local;
 parfor dt = 1:number_delta_t
 
-    if number_delta_t-dt >= maxpts_msd
-        index_start = randperm(number_delta_t-dt, maxpts_msd);
+    if number_timepnts-dt >= maxpts_msd
+        index_start = randperm(number_timepnts-dt, maxpts_msd);
         index_end = index_start+dt;
         delta_coords=(x(:,:,index_start)-x(:,:,index_end));
     else
-        delta_coords=(x(:,:,dt:end)-x(:,:,1:end-dt+1));
+        delta_coords=(x(:,:,1+dt:end)-x(:,:,1 : end-dt ));
     end
   
     % calculate displacement ^ 2 
