02616950cb44a7d71677d6c6cc6db2e62830cdc1
Michael Stefferson <mstefferson@gmail.com>
2016-05-10 17:20:19 -0600

    changeparams makes sures initparams exists in working directory. If it doesn't in copies it

--- src/changeparams_bindobs.m
+++ src/changeparams_bindobs.m
@@ -6,6 +6,7 @@ function [] = changeparams_bindobs( beVec, ffoVec, numtrl, trlind, runind)
 
 if exist('Params.mat','file') == 0 ; 
   fprintf('No params yet, running initparams \n');
+  if exist('initparams.m','file') == 0; cpmatparams; end;
   initparams; 
 else
   load('Params.mat');
