eea66deae87a3ecf3b280d8787fae7b99c14059c
Mike Stefferson <mist7261@vieques.colorado.edu>
2016-06-22 13:10:14 -0600

    Added print statements to run_bindobs

--- run_bindobs.m
+++ run_bindobs.m
@@ -8,70 +8,95 @@
 %tic;
 % clear all;
 %close all;
-addpath('./src');
-StartTime = datestr(now);
-currentdir=pwd;
-fprintf('In dir %s\n',currentdir);
-fprintf('In run_bindobs, %s\n', StartTime);
+try
+  addpath('./src');
+  StartTime = datestr(now);
+  currentdir=pwd;
+  fprintf('In dir %s\n',currentdir);
+  fprintf('In run_bindobs, %s\n', StartTime);
 
-% Check run status
-if exist('StatusRunning.txt','file') ~= 0; 
-%     error('Code is already running in directory');
-elseif exist('StatusFinished.txt','file') ~= 0; 
-    movefile('StatusFinished.txt','StatusRunning.txt');
-else
-    fopen('StatusRunning.txt','w'); 
-end
+  % Check run status
+  if exist('StatusRunning.txt','file') ~= 0; 
+  %     error('Code is already running in directory');
+  elseif exist('StatusFinished.txt','file') ~= 0; 
+      movefile('StatusFinished.txt','StatusRunning.txt');
+  else
+      fopen('StatusRunning.txt','w'); 
+  end
 
-%make output directories if they don't exist
-if exist('./runfiles','dir') == 0; mkdir('./runfiles') ;end;
+  %make output directories if they don't exist
+  if exist('./runfiles','dir') == 0; mkdir('./runfiles') ;end;
 
-%load params. check if it exists, if not, run it, then delete it
-%initparams on tracked, so make it if it's not there
-if exist('Params.mat','file') == 0;
-    if exist('initparams.m','file') == 0;
-        cpmatparams
-    end;
-    initparams
-end
-load Params.mat;
+  %load params. check if it exists, if not, run it, then delete it
+  %initparams on tracked, so make it if it's not there
+  if exist('Params.mat','file') == 0;
+      if exist('initparams.m','file') == 0;
+          cpmatparams
+      end;
+      initparams
+  end
+  load Params.mat;
 
-%display everything
-fprintf('parameters read in\n');
-disp(trialmaster); disp(params); disp(const); disp(modelopt);
+  %display everything
+  fprintf('parameters read in\n');
+  disp(trialmaster); disp(params); disp(const); disp(modelopt);
 
-%build a parameter matrix
-nbe      = length( params.bind_energy_vec );
-nffo     = length( params.ffrac_obst_vec );
-nt       = const.n_trials;
-nparams  = nbe * nffo * nt;
-%param matrix.  (:,1) = run ID (:,2) = binding (:,3) = ffrc obs
-param_mat = zeros( nparams, 3 );
-for i = 1:nt
-    for j = 1:nbe
-        for k = 1:nffo
-            rowind = k + nffo * (j-1) + nffo * nbe * (i - 1);
-            param_mat( rowind, 1 ) = (i-1) + trialmaster.runstrtind;
-            param_mat( rowind, 2 ) = params.bind_energy_vec(j);
-            param_mat( rowind, 3 ) = params.ffrac_obst_vec(k);
-        end
+  %build a parameter matrix
+  nbe      = length( params.bind_energy_vec );
+  nffo     = length( params.ffrac_obst_vec );
+  nt       = const.n_trials;
+  nparams  = nbe * nffo * nt;
+  %param matrix.  (:,1) = run ID (:,2) = binding (:,3) = ffrc obs
+  param_mat = zeros( nparams, 3 );
+  for i = 1:nt
+      for j = 1:nbe
+          for k = 1:nffo
+              rowind = k + nffo * (j-1) + nffo * nbe * (i - 1);
+              param_mat( rowind, 1 ) = (i-1) + trialmaster.runstrtind;
+              param_mat( rowind, 2 ) = params.bind_energy_vec(j);
+              param_mat( rowind, 3 ) = params.ffrac_obst_vec(k);
+          end
+      end
+  end
+
+  % For some reason, param_mat gets "sliced". Create vectors to get arround
+  % this
+  param_RunID = param_mat(:,1);
+  param_bind  = param_mat(:,2);
+  param_ffo   = param_mat(:,3);
+
+  fprintf('Starting paramloop \n')
+  fprintf('nparams = %d\n', nparams)
+  RunTimeID = tic;
+
+  if nparams > 1
+    fprintf('Using parfor to run diffusion model\n');
+    parfor j=1:nparams
+      RunID       = param_RunID(j);
+      bind_energy = param_bind(j);
+      ffrac_obst  = param_ffo(j);
+      
+      pvec=[ffrac_obst params.ffrac_tracer params.slide_barr_height bind_energy]; %parameter vector
+      
+      filestring=['bar',num2str(params.slide_barr_height),'_bind',num2str(bind_energy),...
+          '_fo',num2str(ffrac_obst),'_ft',num2str(params.ffrac_tracer),'_so',...
+          num2str(const.size_obst),'_st',num2str(const.size_tracer),...
+          '_oe',num2str(modelopt.obst_excl),'_ng',...
+          num2str(const.n_gridpoints),'_nt',num2str(const.ntimesteps),...
+          '_nrec', num2str(const.NrecTot),...
+          '_t', num2str(trialmaster.tind),'.',num2str(RunID) ];
+      filename=['data_',filestring,'.mat'];
+      fprintf('%s',filename);
+      
+      %run the model!
+      [tracer,obst] = diffusion_model(pvec,const,modelopt,filename);
+      movefile(filename,'./runfiles');
     end
-end
-
-% For some reason, param_mat gets "sliced". Create vectors to get arround
-% this
-param_RunID = param_mat(:,1);
-param_bind  = param_mat(:,2);
-param_ffo   = param_mat(:,3);
-
-fprintf('Starting paramloop \n')
-RunTimeID = tic;
-
-if nparams > 1
-  parfor j=1:nparams
-    RunID       = param_RunID(j);
-    bind_energy = param_bind(j);
-    ffrac_obst  = param_ffo(j);
+  else
+    fprintf('Running diffusion model once\n');
+    RunID       = param_RunID(1);
+    bind_energy = param_bind(1);
+    ffrac_obst  = param_ffo(1);
     
     pvec=[ffrac_obst params.ffrac_tracer params.slide_barr_height bind_energy]; %parameter vector
     
@@ -83,39 +108,22 @@ if nparams > 1
         '_nrec', num2str(const.NrecTot),...
         '_t', num2str(trialmaster.tind),'.',num2str(RunID) ];
     filename=['data_',filestring,'.mat'];
-    %fprintf(fileid,'%s',filename);
+    fprintf('%s',filename);
     
     %run the model!
     [tracer,obst] = diffusion_model(pvec,const,modelopt,filename);
+    fprintf('Finished %s \n', filename);
     movefile(filename,'./runfiles');
-  end
-else
-  RunID       = param_RunID(1);
-  bind_energy = param_bind(1);
-  ffrac_obst  = param_ffo(1);
-  
-  pvec=[ffrac_obst params.ffrac_tracer params.slide_barr_height bind_energy]; %parameter vector
-  
-  filestring=['bar',num2str(params.slide_barr_height),'_bind',num2str(bind_energy),...
-      '_fo',num2str(ffrac_obst),'_ft',num2str(params.ffrac_tracer),'_so',...
-      num2str(const.size_obst),'_st',num2str(const.size_tracer),...
-      '_oe',num2str(modelopt.obst_excl),'_ng',...
-      num2str(const.n_gridpoints),'_nt',num2str(const.ntimesteps),...
-      '_nrec', num2str(const.NrecTot),...
-      '_t', num2str(trialmaster.tind),'.',num2str(RunID) ];
-  filename=['data_',filestring,'.mat'];
-  %fprintf(fileid,'%s',filename);
-  
-  %run the model!
-  [tracer,obst] = diffusion_model(pvec,const,modelopt,filename);
-  fprintf('Finished %s \n', filename);
-  movefile(filename,'./runfiles');
-end
-RunTime = toc(RunTimeID);
-fprintf('Run time %.2g min\n', RunTime / 60);
-EndTime = datestr(now);
-fprintf('Completed run: %s\n',EndTime);
-fclose('all');
-movefile('StatusRunning.txt','StatusFinished.txt')
-movefile('Params.mat','ParamsLastRun.mat');
-%elapsed_time=toc;
+  end %if nparams > 1
+  RunTime = toc(RunTimeID);
+  fprintf('Run time %.2g min\n', RunTime / 60);
+  EndTime = datestr(now);
+  fprintf('Completed run: %s\n',EndTime);
+  fclose('all');
+  movefile('StatusRunning.txt','StatusFinished.txt')
+  movefile('Params.mat','ParamsLastRun.mat');
+catch err
+ fprintf('%s',err.getReport('extended') );
+end % try catch
+
+
