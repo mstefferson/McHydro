c23ed576e95737b210b33c616c64ca43cf059696
Michael Stefferson <mstefferson@gmail.com>
2016-07-22 11:42:57 -0600

    Scorced earth. Edits to theory. Neeed to edit diffusion_model and computeMSD

--- Dtheory.m
+++ Dtheory.m
@@ -1,18 +1,29 @@
-G = 2;
-%c = linspace(0.1,0.9);
+G = 1000;
 c = [0.1:0.1:0.9];
-Pbe = 1 - min( exp(-G) ,1 );
-Pbo = 1 - min( exp(G) ,1);
+gamma = 10000;
+% alpha = 1 - 2/pi;
+% alpha = 0.5;
+%  alpha = 0.2820;
+% alpha = 0.43;
+
+Pbe = 1 - min( exp(-G) ,1 ); % Probability a hop empty to obst will be blocked
+Pbo = 1 - min( exp(G) ,1);   % Probability a hop obst to empty will be blocked
 
 ffte = (1 - c) ./ ( (1-c) + c .* exp(-G) );
 ffto = ( c * exp(-G) ) ./ ( (1-c) + c .* exp(-G) );
 
-Dch = @(x) (1-x) .* alpha .* ( abs( x - (1-x)*(1-alpha) / (2*alpha) ) -...
-  ( x - (1-x)*(1-alpha) / (2*alpha) ) ) ./ ( (1-x)*(1-alpha) );
+DchInf = @(x,a) (1-x) .* a .* ( abs( x - (1-x)*(1-a) / (2*a) ) -...
+  ( x - (1-x)*(1-a) / (2*a) ) ) ./ ( (1-x)*(1-a) );
 
-D = ffte .* Dch( Pbe .* c ) + ffto .* Dch ( Pbo .* (1-c) );
+DchGam = @(x,gamma,a) (1-x) .* a .* ( abs( x - (1-x)*(1-a) / (2*a) ) -...
+  ( x - (1-x)*(1-a) / (2*a) ) ) ./ ( (1-x)*(1-a) );
 
-plot( c, D )
+D = ffte .* DchInf( Pbe .* c,alpha) + ffto .* DchInf ( Pbo .* (1-c),alpha );
+% Dgam = ffte .* DchGam( Pbe .* c, gamma ) + ffto .* DchGam( Pbo .* (1-c),gamma );
+
+% D2 = ffte .* DchInf( c ) + ffto .* DchInf ( (1-c) );
+
+plot( c, D,'--',c,Dgam,'x' )
 Ax = gca;
 Ax.YLim = [0 1.2];
 Ax.XLim = [0 1];
@@ -20,16 +31,16 @@ ylabel = 'D';
 xlabel = 'ff obstacles';
 
 
-%%% Sanity check %%
-alpha = 1 - 2/pi;
+%% Sanity check %%
+c = linspace(0,1,100);
+% alpha = 1 - 2/pi;
+%alpha = 0.5;
+% alpha = 0.2820;
+alpha = 0.4; %Fits inf data
+alpha = 0.42; % Gives correct perc
+
 gamma = 10000;
 
-f0 = (1-alpha) / ( 1 + (2*gamma-1) * alpha );
-fcgamma = ( ( ( (1-gamma)*(1-c).*f0 + c ).^2 + 4*gamma*(1-c)  *f0 .^ 2 ) .^ (1/2) ...
-  -( (1-gamma)*(1-c)*f0 +c ) ) ./ ...
-  ( 2*gamma*(1-c)*f0 ); 
-fcInf = alpha * ( abs( c - (1-c)*(1-alpha) / (2*alpha) ) -...
-  ( c - (1-c)*(1-alpha) / (2*alpha) ) ) ./ ( (1-c)*(1-alpha) );
 
-Dcgamma = (1-c) .* fcgamma;
-DcInf = (1-c) .* fcInf;
+% figure
+plot(c,DchGam(c,gamma,alpha),c,DchInf(c,alpha));
