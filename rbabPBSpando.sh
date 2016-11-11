965e7aaace48fe26b71152aaaab7c32daef5a9bf
Michael Stefferson <mstefferson@gmail.com>
2016-11-11 10:00:40 -0700

    Added functions to allow for analysis immediately after run

---
+++ rbabPBSpando.sh
@@ -0,0 +1,94 @@
+# Script to run run_bindobs. 
+#
+# Copy this script, customize it and then submit it with the ``qsub''
+# command. For example:
+#
+# cp pbs-template.sh myjob-pbs.sh
+# {emacs|vi} myjob-pbs.sh
+# qsub myjob-pbs.sh
+#
+# PBS directives are fully documented in the ``qsub'' man page. Directives
+# may be specified on the ``qsub'' command line, or embedded in the
+# job script.
+#
+# For example, if you want the batch job to inherit all your environment
+# variables, use the ``V'' switch when you submit the job script:
+#
+# qsub -V myjob-pbs.sh
+#
+# or uncomment the following line by removing the initial ``###''
+
+# Note: group all PBS directives at the beginning of your script.
+# Any directives placed after the first shell command will be ignored.
+
+### Use the bourne shell
+#PBS -S /bin/bash
+
+### Remove only the three initial "#" characters before #PBS
+### in the following lines to enable:
+
+### Optionally set the destination for your program's output
+### Specify localhost and an NFS filesystem to prevent file copy errors.
+#PBS -e /Users/mist7261/McHydro/eo/
+#PBS -o /Users/mist7261/McHydro/eo/
+
+### Specify the number of cpus for your job.  This example will allocate 4 cores
+### using 2 processors on each of 2 nodes.
+#PBS -l nodes=1:ppn=12
+
+### Tell PBS how much memory you expect to use. Use units of 'b','kb', 'mb' or 'gb'.
+#PBS -l mem=24GB
+
+### Tell PBS the anticipated run-time for your job, where walltime=HH:MM:SS
+### NOTE if the simulation runs longer than this it will be killed
+#PBS -l walltime=23:59:59
+
+### Switch to the working directory; by default TORQUE launches processes
+### from your home directory.
+cd $PBS_O_WORKDIR
+echo Working directory is $PBS_O_WORKDIR
+
+# Calculate the number of processors allocated to this run.
+NPROCS=`wc -l < $PBS_NODEFILE`
+
+# Calculate the number of nodes allocated.
+NNODES=`uniq $PBS_NODEFILE | wc -l`
+
+### Display the job context
+echo Running on host `hostname`
+echo Time is `date`
+echo Directory is `pwd`
+echo Using ${NPROCS} processors across ${NNODES} nodes
+echo "Job name: ${PBS_JOBNAME}. Job ID: ${PBS_JOBID}"
+echo "No mail is being sent"
+
+# load matlab
+module load matlab_R2015b
+
+# Run matlab program
+matlab -nodesktop -nosplash \
+  -r  "try, run_analyze_bindobs, catch, exit(1), end, exit(0);" \
+  2>&1 | tee ${PBS_JOBNAME}.out
+echo "Finished. Matlab exit code: $?" 
+echo Time is `date`
+
+# PBS environment variables available in every batch job:
+#
+# $PBS_ENVIRONMENT set to PBS_BATCH to indicate that the job is a batch job; otherwise,
+#                  set to PBS_INTERACTIVE to indicate that the job is a PBS interactive job
+# $PBS_JOBID       the job identifier assigned to the job by the batch system
+# $PBS_JOBNAME     the job name supplied by the user
+# $PBS_NODEFILE    the name of the file that contains the list of nodes assigned to the job
+# $PBS_QUEUE       the name of the queue from which the job is executed
+# $PBS_O_HOME      value of the HOME variable in the environment in which qsub was executed
+# $PBS_O_LANG      value of the LANG variable in the environment in which qsub was executed
+# $PBS_O_LOGNAME   value of the LOGNAME variable in the environment in which qsub was executed
+# $PBS_O_PATH      value of the PATH variable in the environment in which qsub was executed
+# $PBS_O_MAIL      value of the MAIL variable in the environment in which qsub was executed
+# $PBS_O_SHELL     value of the SHELL variable in the environment in which qsub was executed
+# $PBS_O_TZ        value of the TZ variable in the environment in which qsub was executed
+# $PBS_O_HOST      the name of the host upon which the qsub command is running
+# $PBS_O_QUEUE     the name of the original queue to which the job was submitted
+# $PBS_O_WORKDIR   the absolute path of the current working directory of the qsub command
+#
+# End of example PBS script
---
+++ run_analyze_bindobs.m
@@ -0,0 +1,202 @@
+% run_analyze_bindobs()
+% Description: executeable that calls main body of model diffusion_model
+% Program calls loads parameter file or calls initial parameter file if one
+% doesn't exist yet, sets up parallelization, and moves outputs
+% Runs analysis immeditaly after running
+%
+% Authors: LEH, MDB, MWS
+
+function run_bindobs()
+try
+  addpath('./src');
+  StartTime = datestr(now);
+  currentdir=pwd;
+  fprintf('In dir %s\n',currentdir);
+  fprintf('In run_bindobs, %s\n', StartTime);
+
+  % Allocate params
+  params = struct();
+  trialmaster = struct();
+  const = struct();
+  modelopt = struct();
+  
+  % Check run status
+  if exist('StatusRunning.txt','file') ~= 0;
+    %     error('Code is already running in directory');
+  elseif exist('StatusFinished.txt','file') ~= 0;
+    movefile('StatusFinished.txt','StatusRunning.txt');
+  else
+    fopen('StatusRunning.txt','w');
+  end
+  
+  %make output directories if they don't exist
+  if exist('./runfiles','dir') == 0; mkdir('./runfiles') ;end;
+  
+  %load params. check if it exists, if not, run it, then delete it
+  %initparams on tracked, so make it if it's not there
+  if exist('Params.mat','file') == 0;
+    if exist('initparams.m','file') == 0;
+      cpmatparams
+    end;
+    initParams
+  end
+  load Params.mat;
+  
+  % Scramble and shift the seed
+  s = rng('shuffle');
+  rng( s.Seed + trialmaster.seedShift );
+
+  %display everything
+  fprintf('parameters read in\n');
+  disp(trialmaster); disp(params); disp(const); disp(modelopt);
+  
+  %build a parameter matrix
+  nbe      = length( params.bind_energy_vec );
+  nffo     = length( params.ffrac_obst_vec );
+  nsizeobs = length( params.size_obst );
+  nt       = const.n_trials;
+  nparams  = nbe * nffo * nsizeobs * nt;
+  %param matrix.  (:,1) = run ID (:,2) = binding (:,3) = ffrc obs
+  param_mat = zeros( nparams, 4 );
+  for i = 1:nt
+    for j = 1:nbe
+      for k = 1:nffo
+        for l = 1:nsizeobs
+          rowind = 1 + (i-1) + (j-1) * nt + (k-1) * nt * nbe + ...
+            (l-1) * nt * nbe * nffo;
+          param_mat( rowind, 1 ) = (i-1) + trialmaster.runstrtind;
+          param_mat( rowind, 2 ) = params.bind_energy_vec(j);
+          param_mat( rowind, 3 ) = params.ffrac_obst_vec(k);
+          param_mat( rowind, 4 ) = params.size_obst(l);
+        end
+      end
+    end
+  end
+  
+  % For some reason, param_mat gets "sliced". Create vectors to get arround
+  % this
+  param_RunID   = param_mat(:,1);
+  param_bind    = param_mat(:,2);
+  param_ffo     = param_mat(:,3);
+  param_sizeobs = param_mat(:,4);
+  
+  fprintf('Starting paramloop \n')
+  fprintf('nparams = %d\n', nparams)
+  RunTimeID = tic;
+  
+  if nparams > 1
+    fprintf('Using parfor to run diffusion model\n');
+    % Set-up a parpool that's cluster safe
+    % No pool yet
+    if isempty(gcp('nocreate') )
+      fprintf('Creating pool\n')
+      % Initiate a parcluster
+      c = parcluster();
+      % Create temporary directory to parpool data to go
+      clustdir = tempname();
+      mkdir(clustdir)
+      c.JobStorageLocation = clustdir;
+      % Pause for parpool (preventing race conditions) just in case
+      tpause = 1 + 60*rand();
+      fprintf( 'Pausing for %f \n', tpause );
+      pause( tpause );
+      parobj = parpool(c);
+    else % Already pool
+      fprintf('Pool exists\n')
+      parobj = gcp;
+      clustdir = parobj.Cluster.JobStorageLocation;
+      mkdir(clustdir);
+    end
+    
+    fprintf('I have hired %d workers\n',parobj.NumWorkers);
+    fprintf('Temp cluster dir: %s\n', clustdir);
+    
+    parfor j=1:nparams
+      RunID       = param_RunID(j);
+      bind_energy = param_bind(j);
+      ffrac_obst  = param_ffo(j);
+      size_obst   = param_sizeobs(j);
+      
+      pvec=[params.ffrac_tracer, ...
+        params.tr_unbnd_hop_energy, params.tr_bnd_hop_energy,...
+        ffrac_obst, bind_energy, size_obst]; %parameter vector
+      
+      filestring=['unBbar',num2str(params.tr_unbnd_hop_energy),...
+        '_Bbar',num2str(params.tr_bnd_hop_energy),...
+        '_bind',num2str(bind_energy),...
+        '_fo',num2str(ffrac_obst,'%.2f'),'_ft',num2str(params.ffrac_tracer,'%.2f'),...
+        '_so',num2str(size_obst),'_st',num2str(const.size_tracer),...
+        '_oe',num2str(modelopt.obst_excl),'_ng',...
+        num2str(const.n_gridpoints),'_nt',num2str(const.ntimesteps),...
+        '_nrec', num2str(const.NrecTot),...
+        '_t', num2str(trialmaster.tind,'%.2d'),'.',num2str(RunID,'%.2d') ];
+      filename=['data_',filestring,'.mat'];
+      fprintf('%s\n',filename);
+      
+      %run the model!
+      [~,~] = diffusion_model(pvec,const,modelopt,filename);
+      movefile(filename,'./runfiles');
+    end
+    % Clean up tmp
+    delete( [clustdir '/*.mat' ] );
+    delete( [clustdir '/*.txt' ] );
+    if ~isempty( ls(clustdir) )
+      tempDir = ls(clustdir);
+      tempDir = tempDir( ~isspace( tempDir ) );
+      delete( [clustdir '/' tempDir '/*' ] );
+      rmdir( [clustdir '/' tempDir ] );
+    end
+    rmdir(clustdir);
+  else
+    fprintf('Running diffusion model once\n');
+    RunID       = param_RunID(1);
+    bind_energy = param_bind(1);
+    ffrac_obst  = param_ffo(1);
+    size_obst   = param_sizeobs(1);
+    
+    pvec=[params.ffrac_tracer, ...
+      params.tr_unbnd_hop_energy, params.tr_bnd_hop_energy,...
+      ffrac_obst, bind_energy, size_obst]; %parameter vector
+    
+    filestring=['unBbar',num2str(params.tr_unbnd_hop_energy),...
+      '_Bbar',num2str(params.tr_bnd_hop_energy),...
+      '_bind',num2str(bind_energy),...
+      '_fo',num2str(ffrac_obst,'%.2f'),'_ft',num2str(params.ffrac_tracer,'%.2f'),...
+      '_so',num2str(size_obst),'_st',num2str(const.size_tracer),...
+      '_oe',num2str(jjjjjjjjjmodelopt.obst_excl),'_ng',...
+      num2str(const.n_gridpoints),'_nt',num2str(const.ntimesteps),...
+      '_nrec', num2str(const.NrecTot),...
+      '_t', num2str(trialmaster.tind,'%.2d'),'.',num2str(RunID,'%.2d') ];
+    
+    filename=['data_',filestring,'.mat'];
+    fprintf('%s\n',filename);
+    
+    %run the model!
+    [~,~] = diffusion_model(pvec,const,modelopt,filename);
+    fprintf('Finished %s \n', filename);
+    movefile(filename,'./runfiles');
+  end %if nparams > 1
+  runTime = toc(RunTimeID);
+  runHr = floor( runTime / 3600); runTime = runTime - runHr*3600;
+  runMin = floor( runTime / 60);  runTime = runTime - runMin*60;
+  runSec = floor(runTime);
+  fprintf('RunTime: %.2d:%.2d:%.2d (hr:min:sec)\n', runHr, runMin,runSec);
+  EndTime = datestr(now);
+  fprintf('Completed run: %s\n',EndTime);
+  fclose('all');
+  movefile('Params.mat','ParamsLastRun.mat');
+
+  % Now analyze
+  analyzeTimeID = tic;
+  analyze_bindobs(nparams)
+  analyzeTime = toc(analyzeTimeID);
+  analyzeHr = floor( analyzeTime / 3600); analyzeTime = analyzeTime - analyzeHr*3600;
+  analyzeMin = floor( analyzeTime / 60);  analyzeTime = analyzeTime - analyzeMin*60;
+  analyzeSec = floor(analyzeTime);
+  fprintf('AnalyzeTime: %.2d:%.2d:%.2d (hr:min:sec)\n', analyzeHr, analyzeMin,analyzeSec);
+
+  movefile('StatusRunning.txt','StatusFinished.txt')
+ 
+catch err
+  fprintf('%s',err.getReport('extended') );
+end % try catch
