6ac640bf40ac059630624a6ceab8ee3b11114bea
Michael Stefferson <mstefferson@gmail.com>
2017-03-21 09:21:49 -0600

    Added markers and colors to diffMatParamExtract

--- src/diffMatParamExtact.m
+++ src/diffMatParamExtact.m
@@ -1,5 +1,10 @@
+%[diffStruct, param] = ...
+%  diffMatParamExtact( Dmat, pVaryStr, p1want, p2want, p3want )
+%
+% diffMatParamExtact return an organize structure and parameter cell that 
+% make plotting over various parameters much more useful. 
 
-function [Dstruct, param] = ...
+function [diffStruct, param] = ...
   diffMatParamExtact( Dmat, pVaryStr, p1want, p2want, p3want )
 % from str, get parameter info
 if strcmp(pVaryStr,'bdiff')
@@ -75,8 +80,42 @@ numP3 = length(p3want);
 numParams = numP1 * numP2 * numP3;
 % paramMat
 paramMat = combvec( p1want', p2want', p3want' );
+% find max
+numVec = [numP1, numP2, numP3];
+numVecSort = sort( numVec );
+%indsV = [p1Ind, p2Ind, p3Ind];
+indsV = 1:3;
+%nameCell = {p1Name, p2Name, p3Name};
+max1 = numVecSort(3);
+max2 = numVecSort(2);
+max3 = numVecSort(1);
+% only supports two "fixed" variables changing
+if max3 ~= 1
+  error('Too many varying parameters');
+end
+maxInd1 = indsV( max1 ==  numVec );
+maxInd2 = indsV( max2 ==  numVec );
+% extract max vector
+maxTemp = {p1, p2, p3};
+nameTemp = {p1Name, p2Name, p3Name};
+maxVec1 = maxTemp{maxInd1};
+maxVec2 = maxTemp{maxInd2};
+maxName1 = nameTemp{maxInd1};
+maxName2 = nameTemp{maxInd2};
+maxMat = combvec( maxVec1', maxVec2' );
+% Allocate markers and colors
+colormap(['lines(' num2str(numParams) ')']);
+colorArray = colormap;
+colorInds = 1:max1;
+markerArray = ['o','s','^','d','+','x'];
+markerInds = 1:max2;
+indsColorMark = combvec( colorInds, markerInds );
+% allocate
+param.colorMat = colorArray( indsColorMark(1,:), : );
+param.markerVec = markerArray( indsColorMark(2,:) );
+% Allocate diffStruct and paramCell
 paramCell = cell( 1, numParams );
-Dstruct(numParams).pVary = pVary;
+diffStruct(numParams).pVary = pVary;
 % build D struct
 for ii = 1:numParams
   str = [];
@@ -84,50 +123,43 @@ for ii = 1:numParams
   p1temp = paramMat(1,ii);
   p2temp = paramMat(2,ii);
   p3temp = paramMat(3,ii);
+  maxtemp1 = maxMat(1,ii);
+  maxtemp2 = maxMat(2,ii);
   getInd1 = find( Dmat( :, p1Ind ) == p1temp );
   getInd2 = find( Dmat( :, p2Ind ) == p2temp );
   getInd3 = find( Dmat( :, p3Ind ) == p3temp );
   % extra just the info we want
   inds = intersect( getInd1, intersect(getInd2, getInd3) );
   DmatTemp = Dmat(inds, :);
-  if numP1 >  1
-    if isinf(p1temp)
-      str = [str '$$' p1Name ' = \infty $$; '];
+  % build cell
+  if max2 >  1
+    if isinf(maxtemp2)
+      str = [str '$$' maxName2 ' = \infty $$; '];
     else
-      str = [str '$$' p1Name ' = ' num2str( p1temp ) ' $$; '];
+      str = [str '$$' maxName2 ' = ' num2str( maxtemp2 ) ' $$; '];
     end
   end
-  if numP2 >  1
-    if isinf(p2temp)
-      str = [str '$$' p2Name ' = \infty $$; '];
+  if max1 >  1
+    if isinf(maxtemp1)
+      str = [str '$$' maxName1 ' = \infty $$; '];
     else
-      str = [str '$$' p2Name ' = ' num2str( p2temp ) ' $$; '];
+      str = [str '$$' maxName1 ' = ' num2str( maxtemp1 ) ' $$'];
     end
   end
-  if numP3 >  1
-    if isinf(p3temp)
-      str = [str '$$' p3Name ' = \infty $$; '];
-    else
-      str = [str '$$' p3Name ' = ' num2str( p3temp ) ' $$; '];
-    end
-  end
-  % get rid of last ; and spaces
-  if strcmp( str(end), ' ')
-    str = str(1:end-1);
-  end
-  if strcmp( str(end), ';')
-    str = str(1:end-1);
+   % get rid of last ; and spaces
+   if strcmp( str(end-1:end), '; ')
+    str = str(1:end-2);
   end
   % store it
-  Dstruct(ii).Dmat = DmatTemp;
-  Dstruct(ii).pVary = pVary;
-  Dstruct(ii).p1Name = p1Name;
-  Dstruct(ii).p1 = p1temp;
-  Dstruct(ii).p2Name = p2Name;
-  Dstruct(ii).p2 = p2temp;
-  Dstruct(ii).p3Name = p3Name;
-  Dstruct(ii).p3 = p3temp;
-  Dstruct(ii).legStr = str;
+  diffStruct(ii).Dmat = DmatTemp;
+  diffStruct(ii).pVary = pVary;
+  diffStruct(ii).p1Name = p1Name;
+  diffStruct(ii).p1 = p1temp;
+  diffStruct(ii).p2Name = p2Name;
+  diffStruct(ii).p2 = p2temp;
+  diffStruct(ii).p3Name = p3Name;
+  diffStruct(ii).p3 = p3temp;
+  diffStruct(ii).legStr = str;
   paramCell{ii} = str;
 end
 % param stuff
