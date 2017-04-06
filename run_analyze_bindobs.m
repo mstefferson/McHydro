7c571052b579aae07cc5091c48daf8946f514c8a
Michael Stefferson <mstefferson@gmail.com>
2017-04-06 08:41:24 -0600

    run_analyze_bindobs updated to handle ff_tracer -> num_tracer. Changed rbab Walltime to 24hrs

--- run_analyze_bindobs.m
+++ run_analyze_bindobs.m
@@ -1,8 +1,7 @@
-% run_analyze_bindobs()
+% run_bindobs()
 % Description: executeable that calls main body of model diffusion_model
 % Program calls loads parameter file or calls initial parameter file if one
 % doesn't exist yet, sets up parallelization, and moves outputs
-% Runs analysis immeditaly after running
 %
 % Authors: LEH, MDB, MWS
 
@@ -13,7 +12,7 @@ try
   currentdir=pwd;
   fprintf('In dir %s\n',currentdir);
   fprintf('In run_bindobs, %s\n', StartTime);
-
+  
   % Allocate params
   params = struct();
   trialmaster = struct();
@@ -54,45 +53,38 @@ try
   const.rand1 = rand1;
   const.rand2 = rand2;
   fprintf('rand1 = %f rand2 = %f (before after shuffle)\n', rand1, rand2 );
-
+  
   %display everything
   fprintf('parameters read in\n');
   disp(trialmaster); disp(params); disp(const); disp(modelopt);
   
   %build a parameter matrix
-  nbe      = length( params.bind_energy_vec );
-  nffo     = length( params.ffrac_obst_vec );
-  nsizeobs = length( params.size_obst );
-  nt       = const.n_trials;
-  nparams  = nbe * nffo * nsizeobs * nt;
-  %param matrix.  (:,1) = run ID (:,2) = binding (:,3) = ffrc obs
-  param_mat = zeros( nparams, 4 );
-  for i = 1:nt
-    for j = 1:nbe
-      for k = 1:nffo
-        for l = 1:nsizeobs
-          rowind = 1 + (i-1) + (j-1) * nt + (k-1) * nt * nbe + ...
-            (l-1) * nt * nbe * nffo;
-          param_mat( rowind, 1 ) = (i-1) + trialmaster.runstrtind;
-          param_mat( rowind, 2 ) = params.bind_energy_vec(j);
-          param_mat( rowind, 3 ) = params.ffrac_obst_vec(k);
-          param_mat( rowind, 4 ) = params.size_obst(l);
-        end
-      end
-    end
-  end
+  runVec = trialmaster.runstrtind + (0:trialmaster.nt-1);
+  param_mat = combvec(runVec, params.bind_energy_vec, params.ffrac_obst_vec, params.size_obst );
+  [~,nparams] = size(param_mat);
   
   % For some reason, param_mat gets "sliced". Create vectors to get arround
-  % this
-  param_RunID   = param_mat(:,1);
-  param_bind    = param_mat(:,2);
-  param_ffo     = param_mat(:,3);
-  param_sizeobs = param_mat(:,4);
+  param_RunID   = param_mat(1,:);
+  param_bind     = param_mat(2,:);
+  param_ffo      = param_mat(3,:);
+  param_sizeobs = param_mat(4,:);
+  
   
   fprintf('Starting paramloop \n')
   fprintf('nparams = %d\n', nparams)
   RunTimeID = tic;
   
+  % eliminate broadcast warning
+  num_tracer = params.num_tracer;
+  tr_unbnd_diff = params.tr_unbnd_diff;
+  tr_bnd_diff = params.tr_bnd_diff;
+  size_tracer = const.size_tracer;
+  obst_excl = modelopt.obst_excl;
+  n_gridpoints = const.n_gridpoints;
+  dim = const.dim;
+  ntimesteps =  const.ntimesteps;
+  NrecTot = const.NrecTot;
+  tind = trialmaster.tind;
   if nparams > 1
     fprintf('Using parfor to run diffusion model\n');
     % Set-up a parpool that's cluster safe
@@ -106,7 +98,7 @@ try
       mkdir(clustdir)
       c.JobStorageLocation = clustdir;
       % Pause for parpool (preventing race conditions) just in case
-      tpause = 10*rand();
+      tpause = 1 + 60*rand();
       fprintf( 'Pausing for %f \n', tpause );
       pause( tpause );
       parobj = parpool(c);
@@ -119,31 +111,30 @@ try
     
     fprintf('I have hired %d workers\n',parobj.NumWorkers);
     fprintf('Temp cluster dir: %s\n', clustdir);
-    
     parfor j=1:nparams
       % scramble rng in parfor! It's rng is indepedent on ML's current state
       pause(j);
       rng('shuffle');
       fprintf('Parfor j = %d Rand num = %f \n', j, rand() );
-
+      
       RunID       = param_RunID(j);
       bind_energy = param_bind(j);
       ffrac_obst  = param_ffo(j);
       size_obst   = param_sizeobs(j);
       
-      pvec=[params.ffrac_tracer, ...
-        params.tr_unbnd_hop_energy, params.tr_bnd_hop_energy,...
+      pvec=[num_tracer, ...
+        tr_unbnd_diff, tr_bnd_diff,...
         ffrac_obst, bind_energy, size_obst]; %parameter vector
       
-      filestring=['unBbar',num2str(params.tr_unbnd_hop_energy),...
-        '_Bbar',num2str(params.tr_bnd_hop_energy),...
+      filestring=['unD',num2str(tr_unbnd_diff),...
+        '_bD',num2str(tr_bnd_diff,'%.2f'),...
         '_bind',num2str(bind_energy),...
-        '_fo',num2str(ffrac_obst,'%.2f'),'_ft',num2str(params.ffrac_tracer,'%.2f'),...
-        '_so',num2str(size_obst),'_st',num2str(const.size_tracer),...
-        '_oe',num2str(modelopt.obst_excl),'_ng',...
-        num2str(const.n_gridpoints),'_nt',num2str(const.ntimesteps),...
-        '_nrec', num2str(const.NrecTot),...
-        '_t', num2str(trialmaster.tind,'%.2d'),'.',num2str(RunID,'%.2d') ];
+        '_fo',num2str(ffrac_obst,'%.2f'),'_so',num2str(size_obst,'%.2d'),...
+        '_ntrcr',num2str(num_tracer,'%d'),'_st',num2str(size_tracer),...
+        '_oe',num2str(obst_excl),'_ng',num2str(n_gridpoints),...
+        '_dim', num2str(dim),'_nt',num2str(ntimesteps),...
+        '_nrec', num2str(NrecTot),...
+        '_t', num2str(tind,'%.2d'),'.',num2str(RunID,'%.2d') ];
       filename=['data_',filestring,'.mat'];
       fprintf('%s\n',filename);
       
@@ -168,19 +159,19 @@ try
     ffrac_obst  = param_ffo(1);
     size_obst   = param_sizeobs(1);
     
-    pvec=[params.ffrac_tracer, ...
-      params.tr_unbnd_hop_energy, params.tr_bnd_hop_energy,...
+    pvec=[num_tracer, ...
+      tr_unbnd_diff, tr_bnd_diff,...
       ffrac_obst, bind_energy, size_obst]; %parameter vector
     
-    filestring=['unBbar',num2str(params.tr_unbnd_hop_energy),...
-      '_Bbar',num2str(params.tr_bnd_hop_energy),...
+    filestring=['unD',num2str(tr_unbnd_diff),...
+      '_bD',num2str(tr_bnd_diff,'%.2f'),...
       '_bind',num2str(bind_energy),...
-      '_fo',num2str(ffrac_obst,'%.2f'),'_ft',num2str(params.ffrac_tracer,'%.2f'),...
-      '_so',num2str(size_obst),'_st',num2str(const.size_tracer),...
-      '_oe',num2str(modelopt.obst_excl),'_ng',...
-      num2str(const.n_gridpoints),'_nt',num2str(const.ntimesteps),...
-      '_nrec', num2str(const.NrecTot),...
-      '_t', num2str(trialmaster.tind,'%.2d'),'.',num2str(RunID,'%.2d') ];
+      '_fo',num2str(ffrac_obst,'%.2f'),'_so',num2str(size_obst,'%.2d'),...
+      '_ntrcr',num2str(num_tracer,'%d'),'_st',num2str(size_tracer),...
+      '_oe',num2str(obst_excl),'_ng',num2str(n_gridpoints),...
+      '_dim', num2str(dim), '_nt',num2str(ntimesteps),...
+      '_nrec', num2str(NrecTot),...
+      '_t', num2str(tind,'%.2d'),'.',num2str(RunID,'%.2d') ];
     
     filename=['data_',filestring,'.mat'];
     fprintf('%s\n',filename);
@@ -195,12 +186,12 @@ try
   runHr = floor( runTime / 3600); runTime = runTime - runHr*3600;
   runMin = floor( runTime / 60);  runTime = runTime - runMin*60;
   runSec = floor(runTime);
+  fprintf('RunTime: %.2d:%.2d:%.2d (hr:min:sec)\n', runHr, runMin,runSec);
   EndTime = datestr(now);
   fprintf('Completed run: %s\n',EndTime);
   fclose('all');
   movefile('Params.mat','ParamsLastRun.mat');
-
-  % Now analyze
+% Now analyze
   analyzeTimeID = tic;
   analyze_bindobs(nparams)
   analyzeTime = toc(analyzeTimeID);
@@ -218,7 +209,6 @@ try
   fprintf('AnalyzeTime: %.2d:%.2d:%.2d (hr:min:sec)\n', analyzeHr, analyzeMin, analyzeSec);
   fprintf('TotalTime: %.2d:%.2d:%.2d (hr:min:sec)\n', totHr, totMin, totSec);
   movefile('StatusRunning.txt','StatusFinished.txt')
- 
 catch err
   fprintf('%s',err.getReport('extended') );
 end % try catch
