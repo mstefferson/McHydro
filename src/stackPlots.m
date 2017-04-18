74ce66693d403a64b05e3eb408451b0b3150534e
Michael Stefferson <mstefferson@gmail.com>
2017-04-18 17:40:16 -0600

    Added update stackedPlots.

--- src/stackPlots.m
+++ src/stackPlots.m
@@ -31,46 +31,63 @@ function stackPlots( fH, numCols )
 % stackPlots( fig, numCols )
 %
 
-
 % get number of rows and plots
 numPlots = length( fH.Children ) ;
-numRows = numPlots / numCols;
 axVec = cell(1,numPlots);
-
-% grab axis handles in a smart way (like a book), and make it square
+axNum = 0;
+% grab axis handles in a smart way (like a book), and undo squaring
 for ii = 1 : numPlots
-  axVec{ ii } = fH.Children(numPlots + 1 -ii );
-%   maxW = max( axVec{ ii }.Position( 3:4 ) ); 
-%   maxW = max( axVec{ ii }.Position( 3:4 ) ); 
-%   axis( axVec{ ii }, 'square' );
+  if strcmp( fH.Children(numPlots + 1 -ii ).Type, 'axes' )
+    axNum = axNum + 1;
+    axVec{ axNum } = fH.Children(numPlots + 1 -ii );
+    axis( axVec{ axNum }, 'normal')
+    %     hold( axVec{ axNum + 1 }, 'off')
+  end
 end
-
+% get rid of extras
+axVec = axVec( 1:axNum );
+numPlots = axNum;
+numRows = numPlots / numCols;
 % fix stretch
 stretchAmount = (1 - 1 / numRows ) * ...
   ( axVec{end-numCols}.Position(2) - ...
   ( axVec{end}.Position(2) + axVec{end}.Position(4) ) );
 for ii = 1 : numPlots
   axVec{ ii }.Position(4) =  axVec{ ii }.Position(4) + stretchAmount;
-%   axVec{ ii }.Position(3) =  axVec{ ii }.Position(4);
 end
-
 % fix position
 for ii =  numPlots - numCols: - 1: 1
   axVec{ ii }.Position(2) =  axVec{ ii + numCols }.Position(2) + axVec{ ii + numCols }.Position(4);
 end
-
-% Title position 
+% title position 
 aspectRatio = axVec{1}.Position(4) ./ axVec{1}.Position(3); 
 shift = 0.02;
 tX = shift;
 tY = 1 - shift ./ aspectRatio;
-
 % fix appearance
+% title spacing parameter
+titleWiggle = 3/2;
 for ii = 1 : numPlots
-  % axis ticks and labels
-  dTick = axVec{ ii }.YTick(2) - axVec{ ii }.YTick(1);
-  axVec{ ii }.YLim = [axVec{ ii }.YLim(1) axVec{ ii }.YLim(2) + dTick];
-  axVec{ ii }.YTick = axVec{ ii }.YTick(1:end-1);
+  % grab font size for title scaling
+  axVec{ii}.FontUnits = 'normalized';
+  titleSpace = titleWiggle * axVec{1}.FontSize;
+  % axis ticks, labels, mkae room for title
+  if strcmp( axVec{ ii }.YScale, 'linear' )
+    dTick = titleSpace .* ( axVec{ ii }.YLim(2) - axVec{ ii }.YLim(1) );
+    YTickOld = axVec{ ii }.YTick; 
+    lowerYlim = axVec{ ii }.YLim(1);
+    upperYlim = axVec{ ii }.YLim(2) + dTick;
+    axVec{ ii }.YLim = [lowerYlim upperYlim];
+    axVec{ ii }.YTick =  YTickOld;
+  else % log
+    YTickOld = axVec{ ii }.YTick; 
+    lowerYlim = axVec{ ii }.YLim(1);
+    upperYlim = axVec{ ii }.YLim(2) * ...
+      10 ^ (titleSpace* ( log10( axVec{ ii }.YLim(2) ) - log10( axVec{ ii }.YLim(1)) ) );
+    axVec{ ii }.YLim = [lowerYlim upperYlim];
+    axVec{ ii }.YTick =  YTickOld;
+  end
+  % erase labels
   if ii <= ( numRows - 1 ) * numCols
     axVec{ ii }.XTickLabel = {};
   end
@@ -80,4 +97,5 @@ for ii = 1 : numPlots
   axVec{ ii }.Title.VerticalAlignment = 'top';
   axVec{ ii }.Title.HorizontalAlignment = 'left';
   axVec{ ii }.Title.Position =  [tX tY 0];
+  axVec{ ii }.Box =  'on';
 end
