6d7a8aa5d79500dc4a630658a74e324717553fc9
Michael Stefferson <mstefferson@gmail.com>
2017-06-07 10:37:40 -0600

    Cleaned up findHorztlAsymp. Rename variables, added more to description, moved thresholds to the top of file

--- src/findHorztlAsymp.m
+++ src/findHorztlAsymp.m
@@ -1,14 +1,44 @@
-% [output]= findHorztlAsympx,y,erry)
+% [output]= findHorztlAsymp( t, msd, msdErr )
 %   Description: Find the horizontal asymptote, maximum slope, and
 %   intercept (if it can)
-function [output]= findHorztlAsymp(x,y,erry)
+%   inputs: 
+%   t: time 
+%   msd: mean square displacement
+%   msdErr: uncertainty in msd
+function [output]= findHorztlAsymp(t,msd,errMsd)
+% debug
 debugMe = 0;
+% make sure everything is right size and a column vector
+if sum( size(t) ~= size(msd) ) || sum( size(t) ~= size(errMsd) )...
+    || sum( size(msd) ~= size(errMsd) ) 
+  error('inputs are different sizes')
+end
+[r, ~] = size(t);
+if r == 1
+  t = t'; msd = msd'; errMsd = errMsd';
+end
+% tuneable parameters (fit constraints)
+binFac = 1; % tune number of bins
+numBinBulk = 4;
+bulkBinStart = 2; % what bulk bin to start looking at averages
+dataSpreadThreshold = 0.1; % used for finding minimum slope in bulk bin
+finalSlopeCutoff = -0.01; % only count final slopese if they are below this (b.c of noise)
+% if the end slope is small, say it's steady
+% make sure there isn't strange early slope behavior and late unsteady behavior
+thresholdEarly= 0.005;
+decidingFactor = 0.8;
+thresholdSmallEndSlope = 0.02;
+thresholdHugeEndSlope = 0.3;
+% threshold for middle slope
+thresholdMid = 0.03;
+% Don't count noisy end points or start for steady state and max slope
+startInd = 1;
+endInd = 3;
 % add paths
 addpath('./src')
 % Vector is noisey. Bin it to smooth it out
-bindFac = 1;
-binNum = ceil( bindFac * ( log( x(end) ) - log( x(1) ) ) );
-spaceLog = round( logspace( 0, log10( length(x) ), binNum + 1 ) );
+binNum = ceil( binFac * ( log( t(end) ) - log( t(1) ) ) );
+spaceLog = round( logspace( 0, log10( length(t) ), binNum + 1 ) );
 spaceLog = unique( spaceLog );
 binNum = length( spaceLog ) - 1;
 % Store slope and center of each bin
@@ -25,10 +55,10 @@ binLength = zeros( binNum, 1 );
 for ii = 1:binNum
   indStart =  spaceLog(ii) ;
   indEnd = spaceLog(ii+1) ;
-  yTempLog = log( y( indStart:indEnd ) ./ x( indStart:indEnd ) );
-  xTemp =  x( indStart:indEnd );
+  yTempLog = log( msd( indStart:indEnd ) ./ t( indStart:indEnd ) );
+  xTemp =  t( indStart:indEnd );
   xTempLog =  log( xTemp );
-  errTemp = erry ( indStart:indEnd ) ./ ( y(indStart:indEnd) );
+  errTemp = errMsd ( indStart:indEnd ) ./ ( msd(indStart:indEnd) );
   w = 1 ./ ( errTemp .^ 2 );
   ptsTemp = length(yTempLog);
   pfit = fit( xTempLog, yTempLog, 'poly1', 'weights',  w);
@@ -44,29 +74,27 @@ for ii = 1:binNum
 end
 % Do a bulk binnning to make sure you don't start analysis in crap
 % skip first bin
-binBulkSize = floor( binNum / 4 );
-numBinBulk = floor( binNum / binBulkSize );
+binBulkSize = floor( binNum / numBinBulk  ); % number of bulk bins (used to average over noise)
 slopeBinBulk = zeros( numBinBulk, 1 );
 for ii = 1 : numBinBulk
   ind = binBulkSize * (ii - 1) + 1;
   indStart =  spaceLog(ind) ;
   indEnd = spaceLog(ind+binBulkSize) ;
-  yTempLog = log( y( indStart:indEnd ) ./ x( indStart:indEnd ) );
-  xTempLog =  log( x( indStart:indEnd ) );
-  errTemp = erry ( indStart:indEnd ) ./ ( y(indStart:indEnd) );
+  yTempLog = log( msd( indStart:indEnd ) ./ t( indStart:indEnd ) );
+  xTempLog =  log( t( indStart:indEnd ) );
+  errTemp = errMsd ( indStart:indEnd ) ./ ( msd(indStart:indEnd) );
   w = 1 ./ ( errTemp .^ 2 );
   pfit = fit( xTempLog, yTempLog, 'poly1', 'weights',  w);
   slopeBinBulk(ii) = pfit.p1;
 end
 % find mins
 % Get spead of data. If it's small, ignore noisey end
-minData = min ( y./x );
-maxData = max ( y./x );
-meanData = mean( y ./ x);
+minData = min ( msd./t );
+maxData = max ( msd./t );
+meanData = mean( msd ./ t);
 spread = ( maxData - minData ) ./ meanData;
-bulkBinStart = 2;
 % find minimun slope of binner bins
-if spread < 0.1
+if spread < dataSpreadThreshold
   [~, minBulkInd] = min( abs( slopeBinBulk(bulkBinStart:end-1) ) );
 else
   [~, minBulkInd] = min( abs( slopeBinBulk(bulkBinStart:end) ) );
@@ -75,41 +103,23 @@ end
 [~, minIndInBulkBin] = min( abs( slopeBin( ...
   ( minBulkInd ) * binBulkSize  + 1 : (minBulkInd+1)  * binBulkSize ) ) );
 indMostZeroSlope =  minBulkInd * binBulkSize  + minIndInBulkBin;
-% Don't count noisy end points or start for steady state and max slope
-% startInd = find( numPnts > 10, 1 ) ;
-startInd = 1;
-endInd = 3;
-% endInd = round( endFact .* binNum ./ ( log( x(end) ) - log( x(1) ) ) );
 %Store the slope of the first/last bin
 slopeStart = slopeBin(1);
 slopeEnd = slopeBin(end);
-% [slopeMostNeg, indSlopeMostNeg] = min( slopeBin(startInd : indMinSlope) );
-% indSlopeMostNeg =  indSlopeMostNeg + startInd - 1;
 % Start at one and only go to indMinSlope unless it is diverging
-% startInd = 1;
 % finalSlopes =  mean( slopeBin( end - endInd  : end ) );
 finalSlopes =  mean( slopeBin( indMostZeroSlope : end ) );
-slopeCutoff = -0.01;
-if mean( finalSlopes ) < slopeCutoff
+if mean( finalSlopes ) < finalSlopeCutoff
   [slopeMostNeg, indSlopeMostNeg] = min( slopeBin(startInd : end) );
 else
   [slopeMostNeg, indSlopeMostNeg] = min( slopeBin(startInd : indMostZeroSlope) );
 end
-% [slopeMostNeg, indSlopeMostNeg] = min( slopeBin(startInd : indMostZeroSlope) );
-% aveLateSlope =  mean( slopeBin(indMostZeroSlope:end) );
 indSlopeMostNeg =  indSlopeMostNeg + startInd - 1;
-% if the end slope is small, say it's steady
-thresholdEarly= 0.005;
-decidingFactor = 0.8;
 middleIndBulk = ceil( length( slopeBinBulk ) / 2 );
 middleIndBins = round( length( slopeBin ) / 2 );
-% earlyBulkSlope = mean( slopeBinBulk( 1:middleIndBulk) );
-% lateBulkSlope = mean( slopeBinBulk( middleIndBulk:end ) );
 earlyBulkSlope = mean( slopeBinBulk( 1:middleIndBulk) );
 lateBulkSlope = mean( slopeBinBulk( middleIndBulk:end ) );
 dX =  sum( binLength( middleIndBulk:end) );
-thresholdSmallEndSlope = 0.02;
-thresholdHugeEndSlope = 0.3;
 % if late slope if below a critical value, assume it's zero
 critSlopeSmall = log(  ( 1 - thresholdSmallEndSlope / 2  ) ./ ...
   ( 1 + thresholdSmallEndSlope / 2 ) ) ./  dX;
@@ -132,8 +142,7 @@ end
 % Check that it got close to steady state to count: an a relative uncertainty of less than 10 %
 deltaY = binLength(indMostZeroSlope) .* slopeBin(indMostZeroSlope);
 relUncertaintyMin = 2 * abs( (1 - exp( deltaY ) ) ./ ( 1 + exp( -deltaY) ) );
-thresholdMid = 0.03;
-%Check if the last few slopes are worse than the middle
+% Check if the last few slopes are worse than the middle
 % make sure uncertain in asymp from the smallest slope bin isn't too high
 dX = sum( binLength(end-endInd : end) ) ;
 deltaY = finalSlopes * dX;
@@ -154,8 +163,8 @@ if steadyState
   indStart =  spaceLog(indMostZeroSlope) ;
   %indEnd = spaceLog(indMostZeroSlope+1);
   indEnd = spaceLog(end);
-  vec = log( y( indStart:indEnd ) ./ x( indStart:indEnd ) );
-  err = erry( indStart:indEnd ) ./ y( indStart:indEnd ) ;
+  vec = log( msd( indStart:indEnd ) ./ t( indStart:indEnd ) );
+  err = errMsd( indStart:indEnd ) ./ msd( indStart:indEnd ) ;
   w = 1 ./ (err.^2);
   nPnts = length( vec );
   % calculate sums for averages
