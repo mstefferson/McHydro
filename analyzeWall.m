9a5eeca166fc542fb9cf139c47405e0a3d0d0090
Michael Stefferson <mstefferson@gmail.com>
2017-10-24 14:23:14 -0600

    general plot routine. rough start to distance/occ plots

--- analyzeWall.m
+++ analyzeWall.m
@@ -12,9 +12,12 @@ filename = cell( numFiles, 1 );
 type = cell( numFiles, 1);
 bndDiff = zeros( numFiles, 1);
 be = zeros( numFiles, 1 );
-thickness = zeros( numFiles, 1 );
-width = zeros( numFiles, 1);
+param3 = zeros( numFiles, 1 );
+param4 = zeros( numFiles, 1);
 fluxIn = zeros( numFiles, 1);
+occFlag = zeros( numFiles, 1);
+occStore = cell( numFiles, 1);
+occSitesStore = cell( numFiles, 1);
 for ii = 1:numFiles
   % get path to params
   nameTemp = files2analyze(ii).name;
@@ -28,36 +31,63 @@ for ii = 1:numFiles
       type{ii,jj} = obst{jj}{1};
       bndDiff( ii, jj ) = obst{jj}{2}(1);
       be( ii, jj ) = obst{jj}{2}(2);
-      thickness( ii, jj ) = obst{jj}{2}(3);
-      width( ii, jj ) = obst{jj}{2}(4);
+      param3( ii, jj ) = obst{jj}{2}(3);
+      param4( ii, jj ) = obst{jj}{2}(4);
+    elseif strcmp( obst{jj}{1}, 'rand' )
+      type{ii,jj} = obst{jj}{1};
+      bndDiff( ii, jj ) = obst{jj}{2}(1);
+      be( ii, jj ) = obst{jj}{2}(2);
+      param3( ii, jj ) = obst{jj}{2}(3);
+      param4( ii, jj ) = obst{jj}{2}(4);
+    elseif strcmp( obst{jj}{1}, 'teleport' )
+      type{ii,jj} = obst{jj}{1};
+      bndDiff( ii, jj ) = 0;
+      be( ii, jj ) = 0;
+      param3( ii, jj ) = 0;
+      param4( ii, jj ) = 0;
+    elseif strcmp( obst{jj}{1}, 'specloc' )
+      type{ii,jj} = obst{jj}{1};
+      bndDiff( ii, jj ) = obst{jj}{2}(1);
+      be( ii, jj ) = obst{jj}{2}(2);
+      param3( ii, jj ) = obst{jj}{2}(3);
+      param4( ii, jj ) = 0;
     else
       type{ii,jj} = 'none';
       bndDiff( ii, jj ) = 1;
       be( ii, jj ) = 0;
-      thickness( ii, jj ) = 0;
-      width( ii, jj ) = const.n_gridpoints;
+      param3( ii, jj ) = 0;
+      param4( ii, jj ) = const.n_gridpoints;
     end
   end
   if numTemp < numObst
     type{ii,numObst} = 'none';
     bndDiff( ii, numObst ) = 1;
     be( ii, numObst ) = 0;
-    thickness( ii, numObst ) = 0;
-    width( ii, numObst ) = const.n_gridpoints;
+    param3( ii, numObst ) = 0;
+    param4( ii, numObst ) = const.n_gridpoints;
   end
   fluxIn( ii ) = flux;
+  if exist( 'occ', 'var' )
+    occFlag(ii) = 1;
+    occStore{ii} = occ;
+    occSitesStore{ii} = occSites;
+  else
+    occFlag(ii) = 0;
+    occStore{ii} = 0;
+    occSitesStore{ii} = 0;
+  end
 end
 % make table
-T = table( counter, fluxIn );
+T = table( counter, fluxIn, occFlag,occStore, occSitesStore );
 typeT = cell2table( type );
 for ii=1:numObst
   typeTemp = typeT{:,ii};
   Ttemp =  table( bndDiff(:,ii), be(:,ii), ...
-    thickness(:,ii), width(:,ii) );
+    param3(:,ii), param4(:,ii) );
   Tappend = [ typeTemp Ttemp ];
   idstr = num2str( ii, '%d' );
-  varNames = { ['type' idstr], ['bndDiff' idstr], ['be' idstr],...
-    ['thickness' idstr], ['width' idstr] };
+  varNames = { ['type_' idstr], ['bndDiff_' idstr], ['be_' idstr],...
+    ['param3_' idstr], ['param4_' idstr] };
   Tappend.Properties.VariableNames = varNames;
   T = [ T Tappend ];
 end
--- wallScatterPlot.m
+++ wallScatterPlot.m
@@ -1,18 +1,64 @@
 % wall plot
-path2dirs = 'runfiles/20170927LorensGapRequstNoTele';
+function wallScatterPlot( path2dirs )
 % path2dirs = 'runfiles/20171002_LorenTele';
 T = analyzeWall( path2dirs );
+
+if strcmp( unique( T.type_2( ~strcmp(T.type_2,'teleport') ) ), 'wall' )
+  plotFlux( T, 'wall' )
+end
+
+if strcmp( unique( T.type_2( ~strcmp(T.type_2,'teleport') ) ), 'specloc' )
+  plotFlux( T, 'specloc' )
+end
+
+% get inds 
+keyboard
+possDim = [1 2];
+currSubs = T.occSitesStore{1};
+currOcc = T.occStore{1};
+[~, wallDim] = max( currSubs(1,:) );
+varyDim = possDim( possDim ~= wallDim );
+[maxWallPos, maxWallPosInd] = max( currSubs(:,wallDim) );
+varyCenter = currSubs(maxWallPosInd,varyDim);
+
+indsPerpGap = find( currSubs(:, varyDim ) == varyCenter );
+distPerpGap = maxWallPos - currSubs(indsPerpGap, wallDim );
+
+indsParGap = find( currSubs(:,wallDim)  == maxWallPos-1 );
+distParGap  =  varyCenter - currSubs(indsParGap,varyDim);
+
+figure()
+subplot(1,2,1) 
+scatter( distPerpGap, currOcc( indsPerpGap ) );
+title('dist from gap, perp to wall')
+subplot(1,2,2)
+scatter( distParGap, currOcc( indsParGap ) );
+title('dist from gap, par to wall')
+
+%%% sub routines %%%
+function plotFlux(T, plottype)
+if strcmp( plottype, 'wall')
+  xlabelStr = 'thickness, $$t$$';
+  legendStr = '$$ t = $$ ';
+elseif strcmp( plottype, 'specloc' )
+  xlabelStr = 'size, $$ s $$';
+  legendStr = '$$ s = $$ ';
+else
+  error( 'not written')
+end
 % no 2nd obstacle case
-fluxSingle = T.fluxIn( T.width2 == 100 );
+fluxSingle = T.fluxIn( strcmp( T.type_2, 'teleport' ) );
 % get unique vectors
-beUnique = unique( T.be2( T.be2 ~= Inf ) );
-thickUnique = unique( T.thickness2( T.thickness2 > 0 ) );
-bndDiffUnique = unique( T.bndDiff2 );
+inds2analyze = ~strcmp( T.type_2, 'teleport' );
+beUnique = unique( T.be_2( inds2analyze ) );
+thickUnique = unique( T.param3_2( inds2analyze ) );
+bndDiffUnique = unique( T.bndDiff_2( inds2analyze ) );
 numBe = length( beUnique );
 numThick = length( thickUnique );
 numBndDiff = length( bndDiffUnique );
 % set bndDiff identifiers
-bndId = {'-'; '--'};
+% bndId = {'-'; '--'};
+bndId = {'o'; 'sq'};
 colorArrayBe = colormap(['lines(' num2str(length(beUnique)) ')']);
 colorArrayTh = colormap(['lines(' num2str(length(thickUnique)) ')']);
 % plot vs thickness
@@ -22,21 +68,24 @@ legCellBe = cell( numBe * numBndDiff + 1, 1);
 counter = 1;
 for ii = 1:numBndDiff
   bndIdTemp = bndId{ii};
-  bndDiffStr = ['$$ D_{b} = $$' num2str(  bndDiffUnique(ii) )];
+  bndDiffStr = ['$$ D_{b} = $$ ' num2str(  bndDiffUnique(ii) )];
   for jj = 1:numBe
     beStr = ['$$ \Delta G = $$  ' num2str(  beUnique(jj) )];
-    logInd = T.bndDiff2 == bndDiffUnique(ii) & T.be2 == beUnique(jj) & ...
-      T.thickness2 > 0;
+    plotInd = T.bndDiff_2 == bndDiffUnique(ii) & T.be_2 == beUnique(jj) & ...
+      inds2analyze;
     colorIdTemp = colorArrayBe(jj,:);
-    s = plot( thickUnique, T.fluxIn( logInd ) );
-    s.Color = colorIdTemp;
-    s.LineStyle = bndIdTemp;
+    s = scatter( T.param3_2( plotInd ), T.fluxIn( plotInd ));
+    %     s.Color = colorIdTemp;
+    %     s.LineStyle = bndIdTemp;
+    s.MarkerFaceColor = colorIdTemp;
+    s.Marker = bndId{ii};
+    s.SizeData = 50;
     legCellBe{counter} = [ bndDiffStr ' ' beStr ];
     counter = counter + 1;
   end
 end
 plot( thickUnique, fluxSingle * ones( size( thickUnique ) ), 'k:' );
-xlabel('Thickness $$ t $$'); ylabel('$$ j $$');
+xlabel( xlabelStr ); ylabel('$$ j $$');
 legCellBe{counter} = 'none';
 lh = legend( legCellBe, 'location', 'best' );
 lh.Interpreter = 'latex';
@@ -48,15 +97,21 @@ hold
 beNonInf = beUnique( ~isinf( beUnique ) );
 for ii = 1:numBndDiff
   bndIdTemp = bndId{ii};
-  bndDiffStr = ['$$ D_{b} = $$' num2str(  bndDiffUnique(ii) )];
+  bndDiffStr = ['$$ D_{b} = $$ ' num2str(  bndDiffUnique(ii) )];
   for jj = 1:numThick
-    thStr = ['$$ t = $$  ' num2str(  thickUnique(jj) )];
-    logInd = T.bndDiff2 == bndDiffUnique(ii) & T.thickness2 == thickUnique(jj) ...
-      & ~isinf( T.be2 );
+    thStr = [ legendStr num2str(  thickUnique(jj) )];
+    plotInd = T.bndDiff_2 == bndDiffUnique(ii) & T.param3_2 == thickUnique(jj) ...
+      & inds2analyze ;
     colorIdTemp = colorArrayTh(jj,:);
-    s = plot( beNonInf, T.fluxIn( logInd ) );
-    s.Color = colorIdTemp;
-    s.LineStyle = bndIdTemp;
+    % fix sorting issue
+    %     y2plot = T.fluxIn( logInd );
+    %     y2plot( y2plot( beUniqueInds ) )
+    s = scatter( T.be_2( plotInd ) , T.fluxIn( plotInd ) );
+    s.MarkerFaceColor = colorIdTemp;
+    s.Marker = bndId{ii};
+    s.SizeData = 50;
+    %     s.Color = colorIdTemp;
+    %     s.LineStyle = bndIdTemp;
     legCellTh{counter} = [ bndDiffStr ' ' thStr ];
     counter = counter + 1;
   end
@@ -66,4 +121,3 @@ xlabel(' Binding energy $$ \Delta G $$'); ylabel('$$ j $$');
 legCellTh{counter} = 'none';
 lh = legend( legCellTh, 'location', 'best' );
 lh.Interpreter = 'latex';
-
